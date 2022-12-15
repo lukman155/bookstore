3d9f90b95340382d1fed0bfa9f1f8c2f64b771b7
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-12-15 23:11:22 +0100

    Fix: Code Review

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -3,16 +3,6 @@ const REMOVE_BOOK = 'REMOVE_BOOK';
 
 const initialState = [];
 
-export const addBookAction = (book) => ({
-  type: ADD_BOOK,
-  payload: book,
-});
-
-export const removeBookAction = (id) => ({
-  type: REMOVE_BOOK,
-  payload: id,
-});
-
 const booksReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
@@ -23,5 +13,15 @@ const booksReducer = (state = initialState, action) => {
       return state;
   }
 };
+
+export const addBookAction = (book) => ({
+  type: ADD_BOOK,
+  payload: book,
+});
+
+export const removeBookAction = (id) => ({
+  type: REMOVE_BOOK,
+  payload: id,
+});
 
 export default booksReducer;

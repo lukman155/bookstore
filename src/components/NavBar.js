730f5581d3312c680435de848fccbff2fe2c5e59
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2022-12-24 14:27:13 +0100

    Fix:code review errors

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,8 +5,8 @@ const NavBar = () => (
   <div className="nav-con">
     <nav>
       <Link to="/"><h1>Bookstore CMS</h1></Link>
-      <Link to="/">Books</Link>
-      <Link to="/categories">Categories</Link>
+      <Link className="grey" to="/">Books</Link>
+      <Link className="grey" to="/categories">Categories</Link>
     </nav>
     <img className="user-icon" src="https://img.icons8.com/fluency-systems-regular/48/null/user.png" alt="" />
   </div>
